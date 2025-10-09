583ca19d799c16706a863778158683dfb75c6b56
c0mradich <bazankiril@gmail.com>
2025-10-09 08:56:31 +0200

    JS

---
+++ static/js/backend.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"
+dotenv.config()
+const port = provess.env.port
+async function sendNote(note){
+    const res = await fetch(`http://localhost:${port}`, {
+        body: {
+            
+        }
+    })
+    if ( !res.json().success ){
+        console.log(res.json().err)
+    }
+}
